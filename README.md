9b154d2b6ea5ac03b604ab9ddfea1dff7ed6ecdc
gab <gabrielbordeaux@gmail.com>
2016-12-27 19:56:43 -0500

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # password-generator-py
-Passwords easy for humans, hard for computers
+
+## Description
+
+A lot of people with security in mind will use random characters as passwords like `t.J:YuZcTSB=4z*v`.
+[We feel it's secure](https://xkcd.com/936/) because it's complicated. But the password above is as difficult as `abcdefghijkl!123` for a machine to brute force even though it's a lot easier for a user to remember.
+
+This program attempts to create passwords truly difficult for a computer to brute force and easier to remember for a user.
+
+### Each password contains:
+
+ - 3 words from the english dictionary
+ - 1 random number placed at a random position
+ - Random separators between words and numbers
+
+### It is very secure because...
+
+ - Since words length differ, the password length is unpredictable
+ - The separators change randomly
+ - The position of the number change randomly
+ - There are `32,000` (words) `^3` (number of words) `^10` (separator) `^10` (separator) `^10` (separator) `^1000` (numbers) different combinations possible
+
+## Examples
+
+Here are a few passwords that can be generated:
+
+```
+Coaches_Acquires=Dumbbell_908
+28=Haziness_Spatulas+Mortals
+Knights;Decrypts%Oatcakes_320
+Optimise=472+Deterred%Apricots
+375+Hazy%Decorate%Ruler
+Blotched%Dugout_995;Alkyl
+```
+
+## Usage
+
+```
+gab$ ./pass.py 
+844=Chinless=Jewelry+Consumer
+```
+
+## Easy access with your `.profile`
+
+To access the program easily, just add the following alias to your `~/.profile`:
+
+```
+# Generate a password
+alias pass="~/PATH/TO/pass.py"
+```
---
+++ pass.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+# Author: Gabriel Bordeaux (gabfl)
+# Github: https://github.com/gabfl/password-generator-py
+# Version: 1.0
+# Compatible with python 2 & 3
+
+from random import random, randint, choice
+import os
+
+class PasswordGenerator:
+    dictionary = ();
+    intPosition = None;
+    numberOfElements = 4; # 4 = 3 words + 1 integer
+    maxInt = 1000; # Maximum value for the integer
+    separators = ('-', '_', ':', ';', '.', '=', '+', '%', '*'); # List of available separators
+    maxWordLength = 8; # Maximum characters per word
+
+    def __init__(self):
+        self.loadDictionary();
+        self.setIntPosition();
+
+    def getCurrentDir(self):
+        return os.path.dirname(__file__) + '/';
+
+    def loadDictionary(self):
+        """load dictionary into a list."""
+        dicList = [];
+        with open(self.getCurrentDir() + "data/english.csv") as f:
+            lines = f.readlines();
+            # self.dictionary = [line.strip('\n') for line in lines]
+            for line in lines:
+                word = line.strip('\n');
+                if len(word) <= self.maxWordLength:
+                    dicList.append(word);
+        self.dictionary = tuple(dicList);
+
+    def getRandomWord(self):
+        """returns a random word from the dictionary"""
+        return choice(self.dictionary);
+
+    def getRandomSeparator(self):
+        """returns a random separator"""
+        return choice(self.separators);
+
+    def getRandomInt(self):
+        """returns a random number between 0 and self.maxInt"""
+        return randint(0, self.maxInt);
+
+    def setIntPosition(self):
+        """Set the position of the integer in the final password"""
+        self.intPosition = randint(0, self.numberOfElements - 1);
+
+    def generate(self):
+        """Generate a password"""
+        password = '';
+        for i in range(self.numberOfElements):
+            # Add word or integer
+            if i == self.intPosition:
+                password += str(self.getRandomInt());
+            else:
+                password += self.getRandomWord().title();
+
+            # Add separator
+            if i != self.numberOfElements - 1:
+                password += self.getRandomSeparator();
+
+        return password;
+
+    def __call__(self):
+        return self.generate();
+
+# Generate a password
+pw = PasswordGenerator();
+print (pw());
