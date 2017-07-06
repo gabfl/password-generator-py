21e63d301a1e9130d652f00bfc7cf2f4c7ac4ab3
gab <gabrielbordeaux@gmail.com>
2017-07-05 23:12:31 -0400

    Support for PyPI readme

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup (
     name = 'passwordgenerator',
-    version = '1.0.2',
+    version = '1.0.2b',
     description = 'Passwords easy for humans, hard for computers',
+    long_description = long_description,
     author = 'Gabriel Bordeaux',
     author_email = 'pypi@gab.lc',
     url = 'https://github.com/gabfl/password-generator-py/',
