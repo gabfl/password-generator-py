179ba29bbd46fbe589b8bc9fee3903a3ebc7cf77
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2017-07-12 09:48:32 -0400

    PEP8 (#8)

--- src/pwgenerator.py
+++ src/pwgenerator.py
@@ -5,22 +5,24 @@
 # Compatible with python 2.7 & 3
 
 from random import randint, choice
-import os, importlib
+import os
+import importlib
 
 import argparse
 
+
 class PasswordGenerator:
 
-    def __init__(self, argparse = True):
+    def __init__(self, argparse=True):
         # Set default vars
-        self.separators = ('-', '_', ':', ';', '.', '=', '+', '%', '*'); # List of available separators
+        self.separators = ('-', '_', ':', ';', '.', '=', '+', '%', '*');  # List of available separators
 
         # Define options
         self.setOptions(argparse)
 
         # Call initial methods
-        self.loadDictionary();
-        self.setIntPosition();
+        self.loadDictionary()
+        self.setIntPosition()
 
     def setOptions(self, argparse):
         """
@@ -47,7 +49,7 @@ class PasswordGenerator:
             Returns the script directory name to locate the correct dictionary path
         """
 
-        return os.path.dirname(os.path.abspath(__file__)) + '/';
+        return os.path.dirname(os.path.abspath(__file__)) + '/'
 
     def loadDictionary(self):
         """
@@ -72,7 +74,7 @@ class PasswordGenerator:
             if len(word) >= self.options['min_word_length'] and len(word) <= self.options['max_word_length']:
                 break
 
-        return word;
+        return word
 
     def getRandomSeparator(self):
         """
@@ -80,7 +82,7 @@ class PasswordGenerator:
         """
 
         if not self.options['no_special_characters']:
-            return choice(self.separators);
+            return choice(self.separators)
 
         return ''
 
@@ -89,36 +91,37 @@ class PasswordGenerator:
             Returns a random number between 0 and `self.options['max_int_value']`
         """
 
-        return randint(0, self.options['max_int_value']);
+        return randint(0, self.options['max_int_value'])
 
     def setIntPosition(self):
         """
             Set the position of the integer in the final password
         """
 
-        self.intPosition = randint(0, self.options['number_of_elements'] - 1);
+        self.intPosition = randint(0, self.options['number_of_elements'] - 1)
 
     def generate(self):
         """
             Generate a password
         """
 
-        password = '';
+        password = ''
         for i in range(self.options['number_of_elements']):
             # Add word or integer
             if i == self.intPosition:
-                password += str(self.getRandomInt());
+                password += str(self.getRandomInt())
             else:
-                password += self.getRandomWord().title();
+                password += self.getRandomWord().title()
 
             # Add separator
             if i != self.options['number_of_elements'] - 1:
-                password += self.getRandomSeparator();
+                password += self.getRandomSeparator()
 
-        return password;
+        return password
 
     def __call__(self):
-        return self.generate();
+        return self.generate()
+
 
 def main():
     """
@@ -128,17 +131,18 @@ def main():
     # Options
     global args
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--min_word_length", type = int, help = "Minimum length for each word", default = 3)
-    parser.add_argument("-x", "--max_word_length", type = int, help = "Maximum length for each word", default = 8)
-    parser.add_argument("-i", "--max_int_value", type = int, help = "Maximum value for the integer", default = 1000)
-    parser.add_argument("-e", "--number_of_elements", type = int, help = "Number of elements in the password (ie. 4 = 3 words + 1 integer)", default = 4)
-    parser.add_argument("-s", "--no_special_characters",  action='store_true', help = "Do not use special characters")
+    parser.add_argument("-n", "--min_word_length", type=int, help="Minimum length for each word", default=3)
+    parser.add_argument("-x", "--max_word_length", type=int, help="Maximum length for each word", default=8)
+    parser.add_argument("-i", "--max_int_value", type=int, help="Maximum value for the integer", default=1000)
+    parser.add_argument("-e", "--number_of_elements", type=int, help="Number of elements in the password (ie. 4 = 3 words + 1 integer)", default=4)
+    parser.add_argument("-s", "--no_special_characters",  action='store_true', help="Do not use special characters")
     args = parser.parse_args()
 
     # Genrate a password
     pw = PasswordGenerator()
 
     print (pw())
+
 
 def generate():
     """
@@ -147,6 +151,7 @@ def generate():
 
     pw = PasswordGenerator(False)
     return pw()
+
 
 # Generate a password
 if __name__ == "__main__":
