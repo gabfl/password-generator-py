4ed81ea287cf7e46bfab8adf3ea7ec0ba8263b17
gab <gabrielbordeaux@gmail.com>
2017-07-12 11:39:29 -0400

    Add manifest

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ try:
 except(IOError, ImportError):
     long_description = open('README.md').read()
 
-setup (
-    name = 'passwordgenerator',
-    version = '1.0.4',
-    description = 'Passwords easy for humans, hard for computers',
-    long_description = long_description,
-    author = 'Gabriel Bordeaux',
-    author_email = 'pypi@gab.lc',
-    url = 'https://github.com/gabfl/password-generator-py/',
-    license = 'MIT',
-    packages = ['passwordgenerator', 'passwordgenerator.data'],
-    package_dir = { 'passwordgenerator': 'src' },
-    install_requires = ['argparse'], # external dependencies
-    entry_points = {
+setup(
+    name='passwordgenerator',
+    version='1.1',
+    description='Passwords easy for humans, hard for computers',
+    long_description=long_description,
+    author='Gabriel Bordeaux',
+    author_email='pypi@gab.lc',
+    url='https://github.com/gabfl/password-generator-py/',
+    license='MIT',
+    packages=['passwordgenerator', 'passwordgenerator.data'],
+    package_dir={'passwordgenerator': 'src'},
+    install_requires=['argparse'],  # external dependencies
+    entry_points={
         'console_scripts': [
             'passwordgenerator = passwordgenerator.pwgenerator:main',
         ],
