cd59078fd3e6ea85b7acd9bfcf6d04014c0f7220
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2019-02-15 21:33:49 -0500

    Additional removal of inappropriate words (#16)
    * Additional removal of inappropriate words
    
    * Version bump
    
    * Blank line for pep8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup(
     name='passwordgenerator',
-    version='1.3',
+    version='1.4',
     description='Passwords easy for humans, hard for computers',
     long_description=long_description,
     author='Gabriel Bordeaux',
